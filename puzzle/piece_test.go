73361b1d01938babc15f4b45570fbdab8330852d
Steven Linde <stevenlinde@google.com>
2022-11-26 16:13:29 -0800

    testing rotation now

--- puzzle/piece_test.go
+++ puzzle/piece_test.go
@@ -11,68 +11,26 @@ func TestPiece(t *testing.T) {
 	assert := assert.New(t)
 	assert.NotNil(assert)
 
-	piece := NewPiece([]Step{East, East, East})
+	threeNorthPiece := NewPiece([]Step{North, North, North})
+	threeEastPiece := NewPiece([]Step{East, East, East})
+	squarePiece := NewPiece([]Step{East, South, West, North})
 
-	t.Logf("got: %s", piece)
+	assert.NotNil(threeNorthPiece)
+	assert.NotNil(threeEastPiece)
+	assert.NotNil(squarePiece)
 
-	t.Logf("got: %s", South)
-	t.Logf("got: %s", South+1)
-	t.Logf("got: %s", (West+1)%West)
+	rotatedThreeNorthPiece := threeNorthPiece.Rotate()
+	assert.NotEqualValues(threeNorthPiece, rotatedThreeNorthPiece)
+	assert.EqualValues(rotatedThreeNorthPiece, threeEastPiece)
 
-}
-
-/**
-func TestPiece(t *testing.T) {
-
-	assert := assert.New(t)
-	assert.NotNil(assert)
-
-	piece := NewPiece([][]bool{
-		{true, true, true},
-		{true},
-	})
-	assert.NotNil(piece)
-	t.Logf("\n%s", piece)
-
-	rotated := piece.Rotate()
-	t.Logf("\n%s", rotated)
-	assert.NotEqualValues(piece, rotated)
-
-	expectedRotatedShape := NewPiece([][]bool{
-		{false, true, true},
-		{false, false, true},
-		{false, false, true},
-	})
-	assert.EqualValues(expectedRotatedShape, rotated)
+	// do we care that we cant tell a rotated square is the same thing?
+	// rotatedSquarePiece := squarePiece.Rotate()
+	// assert.EqualValues(squarePiece, rotatedSquarePiece)
 
 	// rotate thee more times back to the home orientation
 	for i := 1; i <= 3; i++ {
-		rotated = rotated.Rotate()
-		// TODO we should be able to assert inequality
-		t.Logf("\n%s", rotated)
+		rotatedThreeNorthPiece = rotatedThreeNorthPiece.Rotate()
 	}
-
-	assert.EqualValues(piece, rotated)
-
-	ellPiece := NewPiece([][]bool{
-		{true},
-		{true, true},
-		{false, true},
-	})
-	t.Logf("\n%s", ellPiece)
-	assert.NotNil(ellPiece)
-
-	ellRotated := ellPiece.Rotate()
-	for i := 1; i <= 3; i++ {
-		assert.NotEqualValues(ellPiece, ellRotated)
-		ellRotated = ellRotated.Rotate()
-		t.Logf("\n%s", ellRotated)
-	}
-
-	assert.EqualValues(ellPiece, ellRotated)
-	t.Logf("\n%s", ellPiece.Rotate())
+	assert.EqualValues(rotatedThreeNorthPiece, threeNorthPiece)
 
 }
-
-
-***/
