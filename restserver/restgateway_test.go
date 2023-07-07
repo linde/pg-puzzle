e6df1ab366346e1db35d3e7b08e9954342b3c88a
Steven Linde <stevenlinde@google.com>
2023-07-06 20:29:17 -0700

    this needs some work on the failure modes but making way for swagger

--- restserver/restgateway_test.go
+++ restserver/restgateway_test.go
@@ -46,19 +46,26 @@ func TestRestGateway(t *testing.T) {
 	go rgw.Serve()
 
 	tests := []struct {
-		stopStr       string
+		stops         []puzzle.Loc
 		respCode      int
 		solveExpected bool
 	}{
-		{"", http.StatusOK, true},
-		{"0,0 0,4 4,2", http.StatusOK, true},
-		{"0,4 0,0 4,2", http.StatusOK, true},
+
+		// TODO this always ends up as an empty array in the rpc server.
+
+		{[]puzzle.Loc{}, http.StatusOK, true},
+
+		// TODO there really has to be a better way to create these
+		{[]puzzle.Loc{{R: 0, C: 0}, {R: 0, C: 4}, {R: 4, C: 2}}, http.StatusOK, true},
+
+		// test some failure modes
+		{[]puzzle.Loc{{R: 9999, C: 0}, {R: 0, C: 4}, {R: 4, C: 2}}, http.StatusOK, true},
 	}
 
 	for idx, test := range tests {
 
 		testName := fmt.Sprintf("TestRestGateway(idx:%d){solveExpected:%v,stops:%v",
-			idx, test.solveExpected, test.stopStr)
+			idx, test.solveExpected, test.stops)
 
 		t.Run(testName, func(tt *testing.T) {
 
@@ -66,16 +73,13 @@ func TestRestGateway(t *testing.T) {
 
 			stopSetBytes := []byte{}
 
-			assert.NotNil(test.stopStr)
-			if len(test.stopStr) > 0 {
-				stopSet, stopSetParseErr := puzzle.NewStopSet(test.stopStr)
-				assert.Nil(stopSetParseErr)
-
+			assert.NotNil(test.stops)
+			if len(test.stops) > 0 {
 				// TODO do this somehow better or document it
 				rpcRequest := struct {
-					StopSet puzzle.StopSet
+					StopSet []puzzle.Loc
 				}{
-					StopSet: stopSet,
+					StopSet: test.stops,
 				}
 				stopSetBytes, _ = json.Marshal(rpcRequest)
 			}
